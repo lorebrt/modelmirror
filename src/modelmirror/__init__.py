25be9baf86ac7e41dad56ae0808f63053d583a51
Lorenzo Bretto <lorenzo.bretto@enyr.eu>
2025-11-25 14:59:44 +0000

    added mypy fix to allow class reference import without warning

---
+++ src/modelmirror/__init__.py
@@ -0,0 +1,4 @@
+from .class_provider import ClassReference, ClassRegister
+from .mirror import Mirror
+
+__all__ = ["Mirror", "ClassRegister", "ClassReference"]
---
+++ src/modelmirror/class_provider/__init__.py
@@ -0,0 +1,4 @@
+from .class_reference import ClassReference
+from .class_register import ClassRegister
+
+__all__ = ["ClassRegister", "ClassReference"]
